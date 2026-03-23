76359888ae69fb3474de78c6b701fe333d9c48fc
Jenna Hackett <jenna.hackett96@gmail.com>
2026-03-23 10:34:53 -0600

    comments updated.

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,5 @@
  * 2. 2 buttons (pressables(a new component?)). One for sign up, one for sign in.
  * 3. Sign up button will reroute to signUp page. Upon successful sign up, will reroute to signIn page, will reroute to infoForm page.
  * 4. Sign in button will reroute to  singIn page. Upon successful sign in, will reroute to infoForm page.
- * *************
- * Should signUp/signIn not be successful, user will receive an error asking to try again(?)
+ * ********** NOTE: Should signUp/signIn not be successful, user will receive an error asking to try again(?)
  */
