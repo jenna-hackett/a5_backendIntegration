cd9a214f548434296d9b86bde72466163e42c14d
Jenna Hackett <jenna.hackett96@gmail.com>
2026-03-23 11:01:19 -0600

    context folder with empty authContext file added. config folder with firebase file added.

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@
 /**
  * TODO:
  * 1. Implement welcome message telling user to sign in/sign up.
- * 2. 2 buttons (pressables(a new component?)). One for sign up, one for sign in.
+ * 2. 2 buttons (pressables or a new component?). One for sign up, one for sign in.
  * 3. Sign up button will reroute to signUp page. Upon successful sign up, will reroute to signIn page, will reroute to infoForm page.
  * 4. Sign in button will reroute to  singIn page. Upon successful sign in, will reroute to infoForm page.
  * ********** NOTE: Should signUp/signIn not be successful, user will receive an error asking to try again(?)
