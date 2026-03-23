71c6ae9201be3b683ac39473db1f41a662c1064f
Jenna Hackett <jenna.hackett96@gmail.com>
2026-03-23 10:28:54 -0600

    removed the (tabs) folder & put everything just into (app). restructuring to make more sense with authentication flow.

--- app/infoForm.tsx
+++ app/infoForm.tsx
@@ -10,7 +10,7 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as Yup from "yup";
-import FormError from "../../src/ui/components/FormError";
+import FormError from "../src/ui/components/FormError";
 
 type infoValues = {
   firstName: string;
