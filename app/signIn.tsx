9ce1a26e9962cdbd771d6ed4ea79f033824bd585
Jenna Hackett <jenna.hackett96@gmail.com>
2026-03-23 10:34:17 -0600

    new file (signIn) created. old index code copied over into signIn.tsx.
    index.tsx being converted to home page. TODO comments added for plan.

---
+++ app/signIn.tsx
@@ -0,0 +1,164 @@
+import { Formik } from "formik";
+import {
+  ActivityIndicator,
+  Alert,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as Yup from "yup";
+import FormError from "../src/ui/components/FormError";
+
+type LoginValues = {
+  email: string;
+  password: string;
+};
+
+const loginSchema = Yup.object({
+  email: Yup.string()
+    .email("Enter a valid email")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters")
+    .required("Password is required"),
+});
+
+export default function singIn() {
+  const initialValues: LoginValues = { email: "", password: "" };
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={loginSchema}
+        onSubmit={async (values, { setStatus, resetForm }) => {
+          setStatus(undefined);
+          try {
+            console.log("Form is valid! Data:", values);
+
+            // Create the pop-up
+            Alert.alert(
+              "Login Successful",
+              "Welcome back! You have successfully signed in.",
+              [
+                {
+                  text: "OK",
+                  onPress: () => {
+                    console.log("User clicked OK");
+                    resetForm(); // Clears the inputs after successful login.
+                  },
+                },
+              ],
+            );
+          } catch (err) {
+            setStatus("Login failed. Please check your credentials.");
+          }
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          status,
+        }) => (
+          <View style={styles.container}>
+            <Text style={styles.title}>Sign In</Text>
+
+            <Text style={styles.label}>Email</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your email"
+              autoCapitalize="none"
+              keyboardType="email-address"
+              value={values.email}
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+            ></TextInput>
+            <FormError message={touched.email ? errors.email : undefined} />
+
+            <Text style={styles.label}>Password</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your password"
+              secureTextEntry
+              value={values.password}
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+            ></TextInput>
+            <FormError
+              message={touched.password ? errors.password : undefined}
+            />
+
+            <Pressable
+              style={[styles.button, isSubmitting && styles.buttonDisabled]}
+              onPress={() => handleSubmit()}
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? (
+                <ActivityIndicator />
+              ) : (
+                <Text style={styles.buttonText}>Sign In</Text>
+              )}
+            </Pressable>
+
+            {/* Global Error */}
+            {status && <FormError message={status} />}
+          </View>
+        )}
+      </Formik>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    gap: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "700",
+    marginBottom: 10,
+    paddingVertical: 35,
+  },
+  label: {
+    fontSize: 14,
+    fontWeight: "600",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 10,
+  },
+  button: {
+    width: "100%",
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 8,
+    marginTop: 10,
+    backgroundColor: "black",
+  },
+  buttonDisabled: {
+    opacity: 0.6,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 15,
+    fontWeight: "700",
+  },
+  safeArea: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+});
