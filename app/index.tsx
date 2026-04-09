5f8fcdbb4d564821dedfd284383bfec0724e1349
Aurora Choban <aurorachoban@gmail.com>
2026-04-09 17:45:36 -0600

    major overhaul of index/welcome page, added buttons and functionality to navigate to sign in/sign up pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,73 @@
-// HOME SCREEN.
-/**
- * TODO:
- * 1. Implement welcome message telling user to sign in/sign up.
- * 2. 2 buttons (pressables or a new component?). One for sign up, one for sign in.
- * 3. Sign up button will reroute to signUp page. Upon successful sign up, will reroute to signIn page, will reroute to infoForm page.
- * 4. Sign in button will reroute to  singIn page. Upon successful sign in, will reroute to infoForm page.
- * ********** NOTE: Should signUp/signIn not be successful, user will receive an error asking to try again(?)
- */
-
 import { useRouter } from "expo-router";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const router = useRouter();
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safeArea}>
       <View>
-        <Text>Welcome!</Text>
-        <Text>Please sign up or sign in to access the Information Form.</Text>
-        <Pressable onPress={() => router.push("/infoForm")}>
-          <Text>InfoForm</Text>
-        </Pressable>
-        <Pressable onPress={() => router.push("/signUp")}>
-          <Text>Sign Up</Text>
-        </Pressable>
-        <Pressable onPress={() => router.push("/signIn")}>
-          <Text>Sign In</Text>
-        </Pressable>
+        <Text style={styles.title}>Welcome!</Text>
+        <Text style={styles.text}>
+          Please sign up or sign in to access the Information Form.
+        </Text>
+        <View style={styles.buttons}>
+          <Pressable
+            onPress={() => router.push("/signUp")}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>Sign Up</Text>
+          </Pressable>
+          <Pressable
+            onPress={() => router.push("/signIn")}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>Sign In</Text>
+          </Pressable>
+        </View>
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    fontWeight: "700",
+    textAlign: "center",
+    paddingVertical: 20,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "400",
+    paddingTop: 10,
+    textAlign: "center",
+  },
+  safeArea: {
+    backgroundColor: "white",
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    width: "30%",
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 8,
+    marginTop: 10,
+    backgroundColor: "black",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    paddingVertical: 20,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 15,
+    fontWeight: "700",
+  },
+});
